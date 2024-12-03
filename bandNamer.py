543b5ad02ab256f80b8d4ea77bc36425a58329db
vdr <valeria.do@gmail.com>
2024-12-03 17:22:15 -0600

    create a program to name your band

---
+++ bandNamer.py
@@ -0,0 +1,5 @@
+programGreeting = "Hello! Welcome to the Band Name Generator"
+print(programGreeting)
+birthCity = input("Which city did you grow up in?")
+petName = input("Give me the name of your favorite pet")
+print("The name of your band is " + birthCity + petName)
