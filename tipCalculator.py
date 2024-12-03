082424936be865f188aaef357b7a1fde5009eba6
vdr <valeria.do@gmail.com>
2024-12-03 17:54:24 -0600

    tip calculator

---
+++ tipCalculator.py
@@ -0,0 +1,9 @@
+greeting = "Welcome to the tip calculator!"
+billTotal = input("What was the total bill?\n")
+tipAmount = input("How much tip would you like to give?\n")
+numberOfPatrons = input("How many people are splitting the bill?\n")
+# formula = (billTotal * 1.tipAmount) / numberOfPatrons 
+tipCalculation = int(tipAmount) / 100
+tipTotal = 1 + tipCalculation
+personalBillTotal = (int(billTotal) * tipTotal) / int(numberOfPatrons)  
+stringPersonalBillTotal = print("Each person should pay " + "$" + str(personalBillTotal))
