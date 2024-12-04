605a88ee87b9721c0164d491d1dbb05f7234fc5c
vdr <valeria.do@gmail.com>
2024-12-03 18:03:23 -0600

    secondary version of bill calculator

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,10 +1,16 @@
 greeting = "Welcome to the tip calculator!"
 print(greeting)
-billTotal = input("What was the total bill?\n")
-tipAmount = input("How much tip would you like to give?\n")
+billTotal = input("What was the total bill?\n$")
+tipAmount = input("How much tip would you like to give?\n%")
 numberOfPatrons = input("How many people are splitting the bill?\n")
 # formula = (billTotal * 1.tipAmount) / numberOfPatrons 
 #tipCalculation = int(tipAmount) / 100
 tipTotal = 1 + (int(tipAmount) / 100)
 personalBillTotal = (int(billTotal) * tipTotal) / int(numberOfPatrons)  
 stringPersonalBillTotal = print("Each person should pay " + "$" + str(personalBillTotal))
+
+# More succinct version
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
