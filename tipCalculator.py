2f84f44083164d64842c9424fff42a09deb2e6e0
vdr <valeria.do@gmail.com>
2024-12-04 10:49:15 -0600

    precision cleanup for tip calculator

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,12 +1,12 @@
 greeting = "Welcome to the tip calculator!"
 print(greeting)
-billTotal = input("What was the total bill?\n$")
+billTotal = float(input("What was the total bill?\n$"))
 tipAmount = input("How much tip would you like to give?\n%")
 numberOfPatrons = input("How many people are splitting the bill?\n")
 # formula = (billTotal * 1.tipAmount) / numberOfPatrons 
 #tipCalculation = int(tipAmount) / 100
 tipTotal = 1 + (int(tipAmount) / 100)
-personalBillTotal = (int(billTotal) * tipTotal) / int(numberOfPatrons)  
+personalBillTotal = round(((billTotal) * tipTotal) / int(numberOfPatrons),2) 
 stringPersonalBillTotal = print("Each person should pay " + "$" + str(personalBillTotal))
 
 # More succinct version
