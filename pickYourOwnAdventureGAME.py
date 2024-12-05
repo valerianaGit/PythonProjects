39e1fe0e4f52e6d0f2d7fc03e19cbe913c0f4399
vdr <valeria.do@gmail.com>
2024-12-05 15:02:45 -0600

    refactor

--- pickYourOwnAdventureGAME.py
+++ pickYourOwnAdventureGAME.py
@@ -7,14 +7,17 @@ start = input("Choose left or right \n")
 left = "left"
 right = "right"
 gameOver = "You died. Game Over"
+def first_level():
+        firstLevel = input("You find 3 boxes, Choose which to open between pandora, puppies, void\n")
+        if firstLevel == "pandora":
+            print("You have unleashed the full wrath of the gods " + gameOver)
+        elif firstLevel == "puppies":
+            print("You win at life forever and live happily every after! And you get to have a puppy! ")
+        else:
+            print("That was a weird choice but, ok " + gameOver)
+            
 if start == left:
-    firstLevel = input("You find 3 boxes, Choose which to open between pandora, puppies, void\n")
-    if firstLevel == "pandora":
-        print("You have unleashed the full wrath of the gods " + gameOver)
-    elif firstLevel == "puppies":
-        print("You win at life forever and live happily every after! And you get to have a puppy! ")
-    else:
-        print("That was a weird choice but, ok " + gameOver)
+     first_level()
 else:
     print(gameOver)
 
