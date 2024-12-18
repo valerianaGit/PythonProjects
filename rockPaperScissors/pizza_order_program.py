edba390d71ee976898d7a6cdd48b487e00460bfa
vdr <valeria.do@gmail.com>
2024-12-18 15:42:45 -0600

    working pizza order program

--- rockPaperScissors/pizza_order_program.py
+++ rockPaperScissors/pizza_order_program.py
@@ -8,44 +8,44 @@
 #Add extra cheese for any size pizza (Y or N): +$1
 
 print("Welcome to Python Pizza! Let's work out the bill for your order")
-pizza_size_input = input("Size of your pizza? Enter small, medium or large \n")
+pizza_size_input = input("Size of your pizza? Enter small, medium or large \n").lower()
 print( "size input was " + pizza_size_input)
 size_cost = 25
 def pizza_size_price_func(size_cost):
     if pizza_size_input == "small":
         print("ENTERED SMALL HERE")
         print("SIZE COST BEFORE CHANGE, SHOULD BE 25 " + str(size_cost))
-        size_cost = 15
-        print("SIZE COST BEFORE CHANGE, SHOULD BE 15 " + str(size_cost))
+        return 15
+        #print("SIZE COST BEFORE CHANGE, SHOULD BE 15 " + str(size_cost))
     elif pizza_size_input == "medium":
-         size_cost = 20
+         return 20
     else:
-        size_cost = 25
-pizza_size_price_func(size_cost) # not calling function? or not recognizing size_cost as the same variable?
-print("SIZE COST AFTER CALLING FUNCTION, SHOULD BE 15 " + str(size_cost))
-pepperoni_yes_or_no = False
+        return 25
+pizza_size_price = pizza_size_price_func(size_cost) # not calling function? or not recognizing size_cost as the same variable?
+#print("SIZE COST AFTER CALLING FUNCTION, SHOULD BE 15 " + str(pizza_size_price))
+
 pepperoni_upcharge = 0
-pepperoni_yes_or_no = input("Would you like pepperoni with that? Type True for yes or False for no \n")
-def pepperoni_price_function():
+pepperoni_yes_or_no = input("Would you like pepperoni with that? Type yes or no \n").lower()
+def pepperoni_price_function(pepperoni_yes_or_no, pizza_size_input):
     if pizza_size_input == "small":
-        if pepperoni_yes_or_no == True:
-            pepperoni_upcharge = 2
+        if pepperoni_yes_or_no == "yes":
+            return 2
     elif pizza_size_input == "medium":
-         if pepperoni_yes_or_no == True:
-            pepperoni_upcharge = 3
+         if pepperoni_yes_or_no == "yes":
+            return 3
     elif pizza_size_input == "large":
-            if pepperoni_yes_or_no == True:
-                pepperoni_upcharge = 4
-pepperoni_price_function()
+        if pepperoni_yes_or_no == "yes":
+             return 4
+pepperoni_upcharge = pepperoni_price_function(pepperoni_yes_or_no, pizza_size_input)
 cheese_yes_or_no = False
 cheese_upcharge = 0
-cheese_yes_or_no = input("Add cheese? Type True for yes or False for no \n ")
-def cheese_price_function():
-    if cheese_yes_or_no == True:
-        cheese_upcharge = 2
-cheese_price_function()
-print("Pizza size cost is " + str(size_cost))
+cheese_yes_or_no = input("Add cheese? Type yes or no \n ")
+def cheese_price_function(cheese_yes_or_no):
+    if cheese_yes_or_no == "yes":
+        return 2
+cheese_upcharge = cheese_price_function(cheese_yes_or_no)
+print("Pizza size cost is " + str(pizza_size_price))
 print("Pepperoni upcharge " + str(pepperoni_upcharge))
-print("Cheese upcharge" + str(cheese_upcharge))
-pizza_price = str(int(size_cost) + int(pepperoni_upcharge) + int(cheese_upcharge))
+print("Cheese upcharge " + str(cheese_upcharge))
+pizza_price = str(int(pizza_size_price) + int(pepperoni_upcharge) + int(cheese_upcharge))
 print("Your bill is " + pizza_price)
