002da38fd8c223f4dcb3363bdeebc969c48663e9
vdr <valeria.do@gmail.com>
2024-12-20 13:23:58 -0600

    fix return 0 bugs

--- rockPaperScissors/pizza_order_program.py
+++ rockPaperScissors/pizza_order_program.py
@@ -21,28 +21,32 @@ def pizza_size_price_func(size_cost):
          return 20
     else:
         return 25
-pizza_size_price = pizza_size_price_func(size_cost) # not calling function? or not recognizing size_cost as the same variable?
-#print("SIZE COST AFTER CALLING FUNCTION, SHOULD BE 15 " + str(pizza_size_price))
-
-pepperoni_upcharge = 0
+pizza_size_price = pizza_size_price_func(size_cost) 
 pepperoni_yes_or_no = input("Would you like pepperoni with that? Type yes or no \n").lower()
 def pepperoni_price_function(pepperoni_yes_or_no, pizza_size_input):
     if pizza_size_input == "small":
         if pepperoni_yes_or_no == "yes":
             return 2
+        else:
+            return 0
     elif pizza_size_input == "medium":
          if pepperoni_yes_or_no == "yes":
             return 3
+         else:
+            return 0
     elif pizza_size_input == "large":
         if pepperoni_yes_or_no == "yes":
-             return 4
+            return 4
+        else:
+            return 0
 pepperoni_upcharge = pepperoni_price_function(pepperoni_yes_or_no, pizza_size_input)
-cheese_yes_or_no = False
-cheese_upcharge = 0
+cheese_yes_or_no = "no"
 cheese_yes_or_no = input("Add cheese? Type yes or no \n ")
 def cheese_price_function(cheese_yes_or_no):
     if cheese_yes_or_no == "yes":
         return 2
+    else:
+        return 0
 cheese_upcharge = cheese_price_function(cheese_yes_or_no)
 print("Pizza size cost is " + str(pizza_size_price))
 print("Pepperoni upcharge " + str(pepperoni_upcharge))
