36e51cc471f6000ddb357e12f882954bc2dcd900
vdr <valeria.do@gmail.com>
2024-12-23 11:37:19 -0600

    fizzbuzz challenge

--- PasswordGenerator/for_loops.py
+++ PasswordGenerator/for_loops.py
@@ -24,3 +24,15 @@ x = 0
 for n in range(1,101):
     x+=n
 print(x)
+#==================================================================================== FizzBuzz Game
+#print each number from 1 to 100, if number is divisible by 3 print fizz, if divisible by 5 print buzz , if divisible by both, rint fizzbuzz
+
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+         print(n)
