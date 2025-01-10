a3862355625e506f8dba6e93fdc6526ea63917b5
vdr <valeria.do@gmail.com>
2025-01-10 15:39:05 -0600

    create hangman game

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,23 @@
 # HANGMAN GAME 
-
-word = "demure"
+import random
+words = ["demure", "cutesy", "mindful"]
 score = 6
+word = random.choice(words)
+uncovered_word = ["_"] * len(word)
 
-letter_input = input("Enter a letter\n")
+while score > 0 and "_" in uncovered_word:
+    print("Score ", score)
+    letter_input = input("Enter a letter\n")
+    if letter_input in word:
+        for i, char in enumerate(word):
+            if char == letter_input:
+                uncovered_word[i] = letter_input
+        print("You got it! ", "".join(uncovered_word))
+    else:
+        score -= 1
+        print("Not a match, Your current score is ", score)
+if "_" not in uncovered_word:
+    print("Congratulations! You guessed the word:", word)
+else:
+    print("Score ", score)
+    print("Game over, the word was ", word)
