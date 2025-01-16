1822b3b0afa1e0228c0c1bd8ee3eca47c26b2439
vdr <valeria.do@gmail.com>
2025-01-15 20:11:42 -0600

    editorial gods

--- PasswordGenerator/password_generator.py
+++ PasswordGenerator/password_generator.py
@@ -38,7 +38,7 @@ print(password_letters)
 print(len(password_letters))
 print("Your password is ", password_letters)
 
-#================OPTION 2 REVISED
+#================OPTION 2 REVISED================================================================
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
