495d58d695db2b2b59be75ac2e2a006038d1141e
vdr <valeria.do@gmail.com>
2025-01-15 20:31:56 -0600

    create tasks file for cypher exercise

---
+++ caesar_cypher/task.py
@@ -0,0 +1,17 @@
+# Functions with input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+
+greet_with_name("Jack Bauer")
+
+def greeting_for_two_people(name1, name2):
+    print(f"hello {name1}")
+    print(f"Hello {name2}")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+greet_with(name="Valeria", location="Guayaquil")
