169f74243a5751148436015416e2a2234c63033b
vdr <valeria.do@gmail.com>
2025-01-15 21:11:21 -0600

    ENCODE caesar cypher

--- caesar_cypher/caesar_cypher.py
+++ caesar_cypher/caesar_cypher.py
@@ -34,3 +34,29 @@ encode(input_message, shift_number)
  #   for i, char in message:
   #      new_char = message[i + shift]
         
+#========================================= ENCODE
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabets = alphabet + alphabet + alphabet + alphabet
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+    encrypted_message = ""
+# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+#  by the shift amount and print the encrypted text.
+    for letter in original_text:
+        index = alphabet.index(letter)
+        new_letter = alphabets[index + shift_amount]
+        #print(new_letter)
+        encrypted_message += new_letter
+    print(f"The encrypted message is {encrypted_message}")
+    return encrypted_message
+# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+encrypt("z",9)
+# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
+#  message.
+encrypt(text, shift)
