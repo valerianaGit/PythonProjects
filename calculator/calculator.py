477387e554ded44fa266de9b048001cba74823b4
vdr <valeria.do@gmail.com>
2025-01-28 23:01:39 -0600

    return and input functions

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -4,19 +4,21 @@
 first_number = int(input("What's the first number?\n"))
 operation_chosen = input("\n+\n-\n*\n/\nPick an operation\n")
 second_number = int(input("What's the second number?\n"))
-total = 0
+#total = 0
 #=======================================================================
-if operation_chosen == "+":
-    print(" operation chosen", "+")
-    total = first_number + second_number
-elif operation_chosen == "-":
-    print(" operation chosen", "-")
-    total = first_number - second_number
-elif operation_chosen == "*":
-    print(" operation chosen", "*") 
-    total = first_number * second_number
-elif operation_chosen == "/":
-    print(" operation chosen", "/") 
-    total = first_number / second_number 
-
-print(total)
+def calculator_brain(first_number, second_number, operation_chosen):
+    if operation_chosen == "+":
+        print(" operation chosen", "+")
+        total = first_number + second_number
+    elif operation_chosen == "-":
+        print(" operation chosen", "-")
+        total = first_number - second_number
+    elif operation_chosen == "*":
+        print(" operation chosen", "*") 
+        total = first_number * second_number
+    elif operation_chosen == "/":
+        print(" operation chosen", "/") 
+        total = first_number / second_number 
+    print(total)
+    return total
+calculator_brain(first_number, second_number, operation_chosen)
